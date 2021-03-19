3018b318842797b38c6f162aa485a441cdf31da5
Umesh <umesh@umesh-MacBook-Pro.local>
2021-03-19 15:44:25 +0530

    fesf

--- app/src/main/java/com/example/catapp/util/PaginationScrollListener.kt
+++ app/src/main/java/com/example/catapp/util/PaginationScrollListener.kt
@@ -3,8 +3,8 @@ package com.example.catapp.util
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
-abstract class PaginationScrollListener(var layoutManager: GridLayoutManager) : RecyclerView.OnScrollListener()
-{
+abstract class PaginationScrollListener(var layoutManager: GridLayoutManager) :
+    RecyclerView.OnScrollListener() {
 
     override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
         super.onScrolled(recyclerView, dx, dy)
@@ -22,6 +22,6 @@ abstract class PaginationScrollListener(var layoutManager: GridLayoutManager) : 
     abstract fun isLastPage(): Boolean
     abstract fun isLoading(): Boolean
     abstract fun loadMoreItems()
-    }
+}
 
 
